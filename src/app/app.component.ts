806d4dc9c7f6317dbec2a0e809162eff1e5eaef1
Fábio Naspolini <fabionaspolini@gmail.com>
2022-03-17 00:13:59 -0300

    Add portinari ui 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PoNotificationService } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor (public poNotification: PoNotificationService){}
+  disabled = false;
   title = 'angular-debug';
+
+  showNotification() {
+    this.poNotification.success('PO Notification!')
+    this.disabled = true
+  }
 }
